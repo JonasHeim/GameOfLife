1d0ddd447e450e65c7fdc156e653c45f392bac85
Jonas Heim <heimjonas@yahoo.de>
2019-05-09 23:41:32 +0200

    Function for counting living neighbours added. Rules checking added. Initial Glider structure added. Not yet correct.

--- gameoflife.py
+++ gameoflife.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 import tkinter as tk
+from tkinter import font
 import time
 import math
 import random
 
 scaling = 50
-cell_size = 1 
+cell_size = 10
 
 '''
 Set the window size
@@ -38,20 +39,35 @@ class Application(tk.Frame):
 
         time_begin = time.time()
 
+        #Create handles for cells
         for pos_x in range(num_cell_x):
             for pos_y in range(num_cell_y):
                 self.current_matrix[pos_x][pos_y] = self.draw_layer.create_rectangle(pos_x*cell_size, pos_y*cell_size, pos_x*cell_size+cell_size+1, pos_y*cell_size+cell_size+1, width=0, fill='')
-                self.next_matrix[pos_x][pos_y] = self.current_matrix[pos_x][pos_y]
 
         time_end = time.time()
         
-        print("Initialization of "+str(num_cell_x*num_cell_y)+" cells withing "+str(window_width)+"x"+str(window_height)+" pixels took "+str(time_end-time_begin)+" ms")
+        #Create label for FPS
+        self.font_fps = font.Font(family='Times', size=10, weight='bold')
+        self.label_fps = self.draw_layer.create_text(10, 10, text='0', font=self.font_fps, fill='blue')
 
-        self.after(1, self.update)
+        #Create initial glider pattern
+        self.draw_layer.itemconfig(self.current_matrix[6][5], fill='black')
+        self.draw_layer.itemconfig(self.current_matrix[7][6], fill='black')
+        self.draw_layer.itemconfig(self.current_matrix[5][7], fill='black')
+        self.draw_layer.itemconfig(self.current_matrix[6][7], fill='black')
+        self.draw_layer.itemconfig(self.current_matrix[7][7], fill='black')
+
+
+        print("Initialization of "+str(num_cell_x*num_cell_y)+" cells withing "+str(window_width)+"x"+str(window_height)+" pixels took "+str(time_end-time_begin)+" s")
+
+        self.after(2000, self.update)
 
     def update(self):
-        for i in range(window_width):
-            for j in range(window_height):
+        #Check every cell
+        #TODO
+        time_begin = time.time()
+        for pos_x in range(num_cell_x):
+            for pos_y in range(num_cell_y):
                 '''
                     1. Rule
                         A dead cell is reborn, if it has exact three alive neighours
@@ -60,35 +76,95 @@ class Application(tk.Frame):
                     3. Rule
                         A living cell with exact two or three living neighbours will stay alive
                     4. Rule
-                        A living cell with more than threee living neighbours will die
+                        A living cell with more than three living neighbours will die
                 '''
 
-        for i in range(100):
-            rand_pos_x = random.randint(0, num_cell_x-1)
-            rand_pos_y = random.randint(0, num_cell_y-1)
+                current_neighbours = self.get_living_neighbours(pos_x, pos_y)
+                if 0 != current_neighbours:
+                    print("X: "+str(pos_x)+" Y: "+str(pos_y)+" - "+str(current_neighbours)+" living cells")
 
-            self.draw_layer.itemconfig(self.current_matrix[rand_pos_x][rand_pos_y], fill="#"+("%06x"%random.randint(0,16777215)))
+                if (3 == current_neighbours) or (2 == current_neighbours):
+                    #Cell is reborn/stays alive
+                    self.next_matrix[pos_x][pos_y] = 1
+                elif (2 > current_neighbours) or (3 < current_neighbours):
+                    #Cell will die
+                    self.next_matrix[pos_x][pos_y] = 0
+
+        #Set pixel according to rules
+        for pos_x in range(num_cell_x):
+            for pos_y in range(num_cell_y):
+                if 1 == self.next_matrix[pos_x][pos_y]:
+                    self.draw_layer.itemconfig(self.current_matrix[pos_x][pos_y], fill='black')
+                else:
+                    self.draw_layer.itemconfig(self.current_matrix[pos_x][pos_y], fill='')
+                #Reset next matrix
+                self.next_matrix[pos_x][pos_y] = 0
+
+        time_end = time.time()
+
+        self.draw_layer.itemconfig(self.label_fps, text=str(math.floor(1/(time_end-time_begin))))
         
         self.after(1, self.update)
 
-    def get_neighbours(self, pos_x, pos_y):
-        # Check the four corners at first
-        if(0 == pos_x) and (0 == pos_y):
-            #Top left corner
-            print("Top left corner")
-        elif (num_cell_x == pos_x) and (0 == pos_y):
-            #Top right corner
-            print("Top right corner")
-        elif(0 == pos_x) and (num_cell_y == pos_y):
-            #Bottom left corner
-            print("Bottom left corner")
-        elif(num_cel_x == pos_x) and (num_cell_y == pos_y):
-            #Bottom right corner
-            print("Bottom right corner")
-        else:
-            print("No corner")
+    def get_living_neighbours(self, pos_x, pos_y):
+        #Check all surrounding cells
+        #TL TM TR
+        #LM    RM
+        #BL BM BR
 
-try:
+        retval = 0
+
+        #TL
+        #Don't check if on left edge or on top edge
+        if(0 != pos_y) and (0 != pos_x):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x-1][pos_y-1], "fill"):
+                retval += 1
+
+        #TM
+        #Don't check if on top edge
+        if(0 != pos_y):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x][pos_y-1], "fill"):
+                retval += 1
+            
+        #TR
+        #Don't check if on top edge or on right edge
+        if(0 != pos_y) and ((num_cell_x-1) != pos_x):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x+1][pos_y-1], "fill"):
+                retval += 1
+            
+        #LM
+        #Don't check if on left edge
+        if(0 != pos_x):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x-1][pos_y], "fill"):
+                retval += 1
+            
+
+        #RM
+        #Don't check if on right edge
+        if((num_cell_x-1) != pos_x):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x+1][pos_y], "fill"):
+                retval += 1
+
+        #BL
+        #Don't check if on left edge or bottom edge
+        if(0 != pos_x) and ((num_cell_y-1) != pos_y):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x-1][pos_y+1], "fill"):
+                retval += 1
+
+        #BM
+        #Don't check if on bottom edge
+        if((num_cell_y-1) != pos_y):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x][pos_y+1], "fill"):
+                retval += 1
+
+        #BR
+        #Don't check if on bottom edge or right edge
+        if((num_cell_y-1) != pos_y) and ((num_cell_x-1) != pos_x):
+            if "" != self.draw_layer.itemcget(self.current_matrix[pos_x+1][pos_y+1], "fill"):
+                retval += 1
+
+        return retval
+try:    
     app = Application()
     app.master.title('Game of Life Simulation '+str(window_width)+'x'+str(window_height))
     app.mainloop()
